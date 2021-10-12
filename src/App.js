d5c8055741664f8045639753bbddb2f516af4590
HugoTalledos <hugo.talledos@bpsmart.ai>
2021-10-12 12:43:04 -0500

    Context data added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Form from './components/Form/Form';
 import PreviewList from './components/PreviewList/PreviewList';
+import AppContext from './data/app-data';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const App = () => {
   return (<div className={'main-container'}>
-    <Form/>
-    <PreviewList/>
+    <AppContext>
+      <Form/>
+      <PreviewList/>
+    </AppContext>
  </div>);
 }
 
---
+++ src/data/app-data.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppContext from '../context/app-context';
+
+const reducer = (data, action) => {
+  if (action) {
+    return {
+      ...data,
+      name: action.name,
+      age: action.age,
+      weight: action.weight,
+      sex: action.sex,
+      experience: action.experience,
+      efectivity: action.efectivity,
+      root: action.root,
+      finishRoot: action.finishRoot,
+      separator: action.separator,
+      decimalSeparator: action.decimalSeparator,
+      metric: action.metric,
+      unity: action.unity,
+      columns: action.columns,
+      clear: action.clear,
+      graph: action.graph,
+      delTemp: action.delTemp,
+      disabled: action.disabled,
+    };
+  }
+  return { ...data };
+};
+
+const AppInformation = ({ children }) => {
+  const [data, dispatchData] = React.useReducer(reducer, {
+    name: '',
+    age: 0,
+    weight: 0,
+    sex: '',
+    experience: 0,
+    efectivity: 0,
+    root: '',
+    finishRoot: '',
+    separator: '',
+    decimalSeparator: '',
+    metric: '',
+    unity: '',
+    columns: '',
+    clear: false,
+    graph: false,
+    delTemp: false,
+    disabled: false,
+  });
+  return (
+    <AppContext.Provider value={{ data, dispatchData }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+AppInformation.propTypes = { children: PropTypes.node };
+export default AppInformation;
