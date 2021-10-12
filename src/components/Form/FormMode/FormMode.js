e59d5df2187ba39d0bab2b2648a18a01d3e7df85
HugoTalledos <hugo.talledos@bpsmart.ai>
2021-10-12 12:45:39 -0500

    Save added

--- src/components/Form/FormMode/FormMode.js
+++ src/components/Form/FormMode/FormMode.js
@@ -1,28 +1,43 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Form, Button } from "react-bootstrap";
+import AppContext from "../../../context/app-context";
 import './FormMode.css';
 
 const FormMode = () => {
+  const { data, dispatchData } = useContext(AppContext);
   return(<div className={'main-container__form-mode'}>
     <h2>Modos </h2>
     <div className={'main-container__form-mode--switches'}>
       <Form.Check 
         type="switch"
-        id="custom-switch"
+        disabled={data.graph || data.clear}
+        id="delTemp-switch"
         label="Borrar Temporales"
+        onChange={(e) => dispatchData({ ...data, delTemp: e.target.value === 'on' })}
       />
       <Form.Check 
         type="switch"
         label="Limpieza"
-        id="disabled-custom-switch"
+        id="clear-switch"
+        disabled={data.graph}
+        onChange={(e) => {
+        dispatchData({
+          ...data,
+          clear: e.target.checked,
+          disabled: e.target.checked,
+        })}}
       />
       <Form.Check 
         type="switch"
         label="Gráficas"
-        id="disabled-custom-switch"
+        disabled={data.clear}
+        id="graph-switch"
+        onChange={(e) => dispatchData({
+          ...data, graph: e.target.checked,
+        })}
       />
     </div>
-    <Button variant="success">Agregar</Button>
+    <Button variant="success" onClick={() => console.log(data)}>Agregar</Button>
   </div>);
 };
 
