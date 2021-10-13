7553f8e8a5721fa44b7ee982c20743ccf590675c
HugoTalledos <hugo.talledos@bpsmart.ai>
2021-10-13 09:06:40 -0500

    Unnecessary components deleted

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import FormInfo from './FormInfo/FormInfo';
-import FormConfig from './FormConfig/FormConfig';
-import FormMode from './FormMode/FormMode';
+
 import './Form.css';
 
 const Form = () => {
   return(<div className={'main-container__form'}>
     <FormInfo />
-    <FormConfig />
-    <FormMode />
   </div>)
 };
 
