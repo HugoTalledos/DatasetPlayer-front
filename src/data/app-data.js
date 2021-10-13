bab619ac11f1246b63a175c2abb8094351b26d28
HugoTalledos <hugo.talledos@bpsmart.ai>
2021-10-13 09:05:28 -0500

    context app fixed

--- src/data/app-data.js
+++ src/data/app-data.js
@@ -3,26 +3,11 @@ import PropTypes from 'prop-types';
 import AppContext from '../context/app-context';
 
 const reducer = (data, action) => {
+console.log("🚀 ~ file: app-data.js ~ line 6 ~ reducer ~ action", action)
   if (action) {
     return {
       ...data,
-      name: action.name,
-      age: action.age,
-      weight: action.weight,
-      sex: action.sex,
-      experience: action.experience,
-      efectivity: action.efectivity,
-      root: action.root,
-      finishRoot: action.finishRoot,
-      separator: action.separator,
-      decimalSeparator: action.decimalSeparator,
-      metric: action.metric,
-      unity: action.unity,
-      columns: action.columns,
-      clear: action.clear,
-      graph: action.graph,
-      delTemp: action.delTemp,
-      players: [],
+      player: action.player,
     };
   }
   return { ...data };
@@ -30,23 +15,24 @@ const reducer = (data, action) => {
 
 const AppInformation = ({ children }) => {
   const [data, dispatchData] = React.useReducer(reducer, {
-    name: '',
-    age: 0,
-    weight: 0,
-    sex: '',
-    experience: 0,
-    efectivity: 0,
-    root: '',
-    finishRoot: '',
-    separator: '',
-    decimalSeparator: '',
-    metric: '',
-    unity: '',
-    columns: '',
-    clear: false,
-    graph: false,
-    delTemp: false,
-    players: [],
+    player: {
+      name: '',
+      age: 0,
+      weight: 0,
+      sex: '',
+      experience: 0,
+      efectivity: 0,
+      root: '',
+      rootFinish: '',
+      separator: ',',
+      decimalSeparator: '.',
+      metric: '',
+      unity: '',
+      columns: '',
+      clear: false,
+      graph: false,
+      temp: false,
+    }
   });
   return (
     <AppContext.Provider value={{ data, dispatchData }}>
