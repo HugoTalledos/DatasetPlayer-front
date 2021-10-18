e186b902c8729fe284134a9900bf3cc743b9eac4
HugoTalledos <hugo.talledos@bpsmart.ai>
2021-10-18 00:09:47 -0500

    Navbar added

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import Form from './components/Form/Form';
 import PreviewList from './components/PreviewList/PreviewList';
 import Notification from './components/Alert/Alert';
 import AppContext from './data/app-data';
+import Navbar from './components/NavbarForm/NavbarForm';
 import PushNotificationContext from './data/notification-data';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const App = () => {
-  return (<div className={'main-container'}>
-    <AppContext>
-      <PushNotificationContext>
+  return (<>
+  <PushNotificationContext>
+    <Navbar/>
+    <div className={'main-container'}>
+      <AppContext>
         <Form/>
         <PreviewList/>
         <Notification/>
-      </PushNotificationContext>
-    </AppContext>
- </div>);
+      </AppContext>
+    </div>
+  </PushNotificationContext>
+  </>);
 }
 
 export default App;
