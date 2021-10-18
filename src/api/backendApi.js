0c4a7227650a2ad21b080450578f7dc9536e9271
HugoTalledos <hugo.talledos@bpsmart.ai>
2021-10-18 18:06:27 -0500

    Graph mode and clear mode added

--- src/api/backendApi.js
+++ src/api/backendApi.js
@@ -1,9 +1,17 @@
 import { api, getResponseData, escalateError } from './index';
 
 export default class BackendPlayer {
-  static startProcessPlayer(data) {
-    return api.post('processPlayer', data)
+  static startGraphMode(data, token) {
+    return api.post('graphMode', data,
+      { headers: { Authorization: localStorage.getItem('token') } })
       .then(getResponseData)
       .catch(escalateError);
   };
+
+  static startClearMode(data) {
+    return api.post('clearMode', data,
+    { headers: { Authorization: localStorage.getItem('token') } })
+    .then(getResponseData)
+    .catch(escalateError);
+  }
 };
