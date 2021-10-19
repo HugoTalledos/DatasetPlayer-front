8c20ede17d0452f0852309438c0cf0d82d24bfc1
HugoTalledos <hugo.talledos@bpsmart.ai>
2021-10-18 20:10:30 -0500

    Modal component added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import Form from './components/Form/Form';
 import PreviewList from './components/PreviewList/PreviewList';
 import Notification from './components/Alert/Alert';
+import ModalAlert from './components/Alert/ModalAlert';
 import AppContext from './data/app-data';
 import Navbar from './components/NavbarForm/NavbarForm';
 import PushNotificationContext from './data/notification-data';
+import ModalContext from './data/modal-data';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const App = () => {
   return (<>
-  <PushNotificationContext>
-    <Navbar/>
-    <div className={'main-container'}>
-      <AppContext>
-        <Form/>
-        <PreviewList/>
-        <Notification/>
-      </AppContext>
-    </div>
-  </PushNotificationContext>
+  <ModalContext>
+    <PushNotificationContext>
+      <Navbar/>
+      <div className={'main-container'}>
+        <AppContext>
+          <Form/>
+          <PreviewList/>
+          <Notification/>
+          <ModalAlert/>
+        </AppContext>
+      </div>
+    </PushNotificationContext>
+  </ModalContext>
   </>);
 }
 
