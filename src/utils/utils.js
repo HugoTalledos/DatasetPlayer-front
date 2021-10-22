0098484a9b676b2efee8a495ad6cc0b2cd728a14
HugoTalledos <hugo.talledos@bpsmart.ai>
2021-10-21 22:38:01 -0500

    Validate fields util added

---
+++ src/utils/utils.js
@@ -0,0 +1,40 @@
+export default class Utils {
+  static validateField(data, action) {
+    const {
+      documentNumber, listFiles, separator,
+      decimalSeparator, metric, unity, columns,
+      name, age, weight, sex, experience,
+      efectivity, columnTime, gestureType
+    } = data;
+
+    switch (action) {
+      case 'clear':
+        return (!documentNumber || documentNumber === 0);
+      case 'graph':
+        return (!documentNumber || documentNumber.length === 0
+          || !listFiles || listFiles.length === 0
+          || !separator || separator.length === 0
+          || !decimalSeparator || decimalSeparator.length === 0
+          || !metric || metric.length === 0
+          || !unity || unity.length === 0
+          || !columns || columns.length === 0);
+      default:
+        return (!documentNumber || documentNumber === 0
+          || !name || name.length === 0
+          || !age || age.length === 0
+          || !weight || weight.length === 0
+          || !sex || sex.length === 0
+          || !experience || experience.length === 0
+          || !efectivity || efectivity.length === 0
+          || !listFiles || listFiles.length === 0
+          || !separator || separator.length === 0
+          || !decimalSeparator || decimalSeparator.length === 0
+          || !metric || metric.length === 0
+          || !unity || unity.length === 0 
+          || !columns || columns.length === 0
+          || !columnTime || columnTime.length === 0
+          || !gestureType || gestureType.length === 0
+          );
+    }  
+  }
+}
