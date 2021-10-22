f039ef0c42fcc794220443204dbcdae3a88dc84d
HugoTalledos <hugo.talledos@bpsmart.ai>
2021-10-21 22:46:50 -0500

    App refactor

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
-import { Router, Route } from 'react-router';
-import FormConfig from './components/MainScreen/MainScreen';
-import Report from './components/Report/Report';
 import Navbar from './components/NavbarForm/NavbarForm';
 import Notification from './components/Alert/Alert'
 import ModalAlert from './components/Alert/ModalAlert'
 import PushNotificationContext from './data/notification-data';
-import history from './history';
+
 import ModalContext from './data/modal-data';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MainContainer from './components/MainContainer/MainContainer';
 
 const App = () => {
   return (<>
-  <Router history={history}>
     <ModalContext>
       <PushNotificationContext>
         <Navbar/>
-          <Route exact path={'/'}>
-            <FormConfig/>
-          </Route>
-          <Route exact path={'/reportes'}>
-            <Report/>
-          </Route>
-          <Notification/>
-          <ModalAlert/>
+        <MainContainer/>
+        <Notification/>
+        <ModalAlert/>
       </PushNotificationContext>
     </ModalContext>
-  </Router>
   </>);
 }
 
